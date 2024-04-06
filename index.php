eec62481dca50d4e372a8fd0ee7a5d8d682a76d5
ElliNet13 <elliotmoose4@gmail.com>
2024-04-06 01:38:32 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title>Run Python</title>
+  <script>
+    function shell() {
+      var code = document.getElementById('code').value;
+      window.location.href = `run.php?shell=${encodeURIComponent(code)}`;
+    }
+  </script>
+</head>
+<body>
+  <h1>Python runner</h1>
+  <br><button onclick="shell()">Run</button>
+  <br><input id="code" type="text">
+</body>
+</html>
---
+++ run.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Python run console</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
+</head>
+
+<body>
+  <?php
+  if (isset($_GET['shell'])) {
+      $param = $_GET['shell'];
+
+      if (!empty($param) && !strstr($param, "unlink") && !strstr($param, "fopen")) {
+          echo "<py-script>$param</py-script>";
+      } else {
+          http_response_code(400);
+          echo "Invalid command";
+      }
+  } else {
+      http_response_code(400);
+      echo "Shell parameter missing";
+  }
+  ?>
+  <script src="script.js"></script>
+  <script defer src="https://pyscript.net/latest/pyscript.js"></script>
+</body>
+
+</html>
